fbbae0e3e887b672379b590d327f7b8084d44731
Shelley Fong <s.fong@auckland.ac.nz>
2021-11-30 15:52:13 +1300

    Adding files

---
+++ main.py
@@ -0,0 +1,26 @@
+
+import bibtexparser
+
+path = 'C:/Users/sfon036/Desktop/work_files/PhysiomeSubmissions/TEX_Mathematical model of excitation-contraction in a uterine smooth muscle cell/'
+with open(path+'sample.bib') as f:
+    bib_database = bibtexparser.load(f)
+
+
+# write out title, journal_title, author, volume, issue year
+xr_list = ['article_title', 'journal_title', 'author', 'volume', 'issue', 'cYear']
+bib_list = ['title', 'journal', 'author', 'volume', 'issue', 'year']
+fields = dict()
+fields = {xr_list[i]: bib_list[i] for i in range(len(xr_list))}
+
+XR_output = path + 'crossref_citation_list.xml'
+with open(XR_output, 'w') as w:
+    w.write('<citation_list>\n')
+    for count, entry in enumerate(bib_database.entries):
+        w.write('<citation key='+str(count)+'>\n',)
+        for field in fields:
+            try:
+                w.write('\t<%s>' % field + entry['%s' % fields[field]] + '</%s>\n' % field)
+            except:
+                pass
+        w.write('/<citation>\n')
+    w.write('/<citation_list>\n')
